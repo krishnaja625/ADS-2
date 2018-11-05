fa2f0edff9873d7592f0750c7e288e806f211448
krishnaja625 <krishnaja625@msitprogram.net>
2018-11-05 21:51:54 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -15,16 +15,16 @@ final class Solution {
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ver = Integer.parseInt(sc.nextLine());
-        int edges = Integer.parseInt(sc.nextLine());
-        EdgeWeightedGraph ewg = new EdgeWeightedGraph(ver);
+        int numberOfvertices = Integer.parseInt(sc.nextLine());
+        int numberOfedges = Integer.parseInt(sc.nextLine());
+        EdgeWeightedGraph ewgraph = new EdgeWeightedGraph(numberOfvertices);
         while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(" ");
             Edge e = new Edge(Integer.parseInt(tokens[0]),
                 Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
-            ewg.addEdge(e);
+            ewgraph.addEdge(e);
         }
-        KruskalMST kmst = new KruskalMST(ewg);
-        System.out.format("%.5f", kmst.weight());
+        KruskalMST mst = new KruskalMST(ewgraph);
+        System.out.format("%.5f", mst.weight());
     }
 }
