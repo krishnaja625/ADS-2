3f0c0434f1a47f3a1e7ed557865ae4489702a7ca
krishnaja625 <krishnaja625@msitprogram.net>
2018-11-06 18:33:14 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2-assignments/m8/1. Shortest Paths/Solution.java
@@ -17,31 +17,31 @@ final class Solution {
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<String, Integer> map
+        HashMap<String, Integer> mapping
         = new HashMap<String, Integer>();
         String[] tokens = sc.nextLine().split(" ");
         int edges = Integer.parseInt(tokens[1]);
         String[] vertices = sc.nextLine().split(" ");
         for (int i = 0; i < vertices.length; i++) {
-            map.put(vertices[i], i);
+            mapping.put(vertices[i], i);
         }
-        Edge e;
+        Edge edge;
         EdgeWeightedGraph ewg
         = new EdgeWeightedGraph(vertices.length);
         for (int i = 0; i < edges; i++) {
             String[] directPath = sc.nextLine().split(" ");
-            e = new Edge(map.get(directPath[0]),
-                               map.get(directPath[1]),
+            edge = new Edge(mapping.get(directPath[0]),
+                               mapping.get(directPath[1]),
                                Double.parseDouble(directPath[2]));
-            ewg.addEdge(e);
+            ewg.addEdge(edge);
         }
         int queries = Integer.parseInt(sc.nextLine());
-        DijkstraSP dsp;
+        DijkstraSP d;
         for (int i = 0; i < queries; i++) {
             String[] check = sc.nextLine().split(" ");
-            int source = map.get(check[0]);
-            dsp = new DijkstraSP(ewg, source);
-            System.out.println((int) dsp.distance(map.get(check[1])));
+            int source = mapping.get(check[0]);
+            d = new DijkstraSP(ewg, source);
+            System.out.println((int) d.distance(mapping.get(check[1])));
         }
     }
 }
